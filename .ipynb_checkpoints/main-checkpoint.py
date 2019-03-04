b2eb44d7412261aac5f77756eb8bb50dc8826528
Rohit T <rohitsrivatsav@gmail.com>
2019-03-04 09:06:37 -0600

    checkin with istructor code

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -3,6 +3,6 @@ from src.bayesdice import BayesDice
 bd = BayesDice()
 bd.choose_die()
 
-for i in range(10):
+for i in range(3):
     roll = bd.roll_die()
     bd.update_priors(roll)
--- src/bayesdice.py
+++ src/bayesdice.py
@@ -1,15 +1,66 @@
+# from random import choice
+# import numpy as np
+
+
+# class BayesDice:
+#     def __init__(self):
+#         self.dice = 0
+#         self.dices = [4, 6, 8, 12, 20]
+#         self.prob = {}
+#         self.dice_nums = {4: [l for l in range(1, 5)], 6: [l for l in range(1, 7)], 8: [l for l in range(1, 9)], 12: [l for l in range(1, 13)], 20: [l for l in range(1, 21)]}
+
+#     def choose_die(self):
+#         self.dice = choice(self.dices)
+#         print("selected dice ")
+#         print(self.dice)
+#         return self.dice
+
+#     def roll_die(self):
+#         print("rolled dice ")
+#         c = choice([l for l in range(self.dice + 1)])
+#         print(c)
+#         return c
+
+#     def update_priors(self, roll):
+#         for dice, values in self.dice_nums.items():
+#             if(roll in values):
+#                 p = 1 / len(values)
+#                 self.prob[dice] = p
+#             else:
+#                 self.prob[dice] = 0.0
+#         print(self.prob)
+
+#         # self.prob.append(roll)
 import random
 
 
 class BayesDice:
+    # ------------------------------------------------------------ #
     def __init__(self):
-        pass
+        self.dice = [4, 6, 8, 12, 20]
+        self.data = {die: 0.20 for die in self.dice}
+    # ------------------------------------------------------------ #
 
     def choose_die(self):
-        pass
+        self.die = random.choice(self.dice)
+    # ------------------------------------------------------------ #
 
     def roll_die(self):
-        pass
+        return random.randint(1, self.die)
+    # ------------------------------------------------------------ #
 
     def update_priors(self, roll):
-        pass
+        print('before update:', self.data)
+        denominator = list(map(lambda die: (0 if roll > die else (1 / die)) * self.data[die], self.dice))
+        self.data = {self.dice[i]: numerator / sum(denominator) for i, numerator in enumerate(denominator)}
+        self.debug(roll, denominator)
+    # ------------------------------------------------------------ #
+
+    def debug(self, roll, denominator):
+        print('die:', self.die, 'roll:', roll)
+        print('denominator:', denominator)
+        print('data:', self.data)
+        print('sum of priors:', sum(self.data.values()))
+        print('sum of denominator:', sum(denominator))
+        print('-' * 50)
+    # ------------------------------------------------------------ #
